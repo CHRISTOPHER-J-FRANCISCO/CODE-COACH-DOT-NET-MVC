cfdb3689ed0687f67fac8f10625dc0452a99fd2b
Christopher <cjfg00gl3@gmail.com>
2024-09-18 15:45:25 -0500

    CREATE SignInModel.cs to store the username and password

---
+++ Models/SignInModel.cs
@@ -0,0 +1,7 @@
+public class SignInModel
+{
+
+    public required string Username {get; set;} // where the username will be stored
+
+    public required string Password {get; set;} // where the password will be stored
+}

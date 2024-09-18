d76f1202ba3353516a8123220d9b56a3a0269453
Christopher <cjfg00gl3@gmail.com>
2024-09-18 17:23:04 -0500

    DELETE SignInModel.cs will be using jquery for front end interaction

--- Models/SignInModel.cs
+++
@@ -1,7 +0,0 @@
-public class SignInModel
-{
-
-    public required string Username {get; set;} // where the username will be stored
-
-    public required string Password {get; set;} // where the password will be stored
-}
